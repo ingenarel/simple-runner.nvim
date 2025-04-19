0579629f0f13b73f3cd21148633aada1ce5fc9c2
ingenarel <ingenarelitems@gmail.com>
2025-04-20 01:25:01 +0600

    (misc): add format

--- lua/simple-runner/init.lua
+++ lua/simple-runner/init.lua
@@ -1,50 +1,50 @@
 local m = {}
 
 function m.setup(opts)
-	m.heightPercentage = opts.heightPercentage or 70
-	m.widthPercentage = opts.widthPercentage or 80
-	m.browser = opts.browser
+    m.heightPercentage = opts.heightPercentage or 70
+    m.widthPercentage = opts.widthPercentage or 80
+    m.browser = opts.browser
 end
 
 function m.run(closeOnExit)
-	local currentFiletype = vim.bo.filetype
+    local currentFiletype = vim.bo.filetype
 
-	vim.cmd("w")
+    vim.cmd("w")
 
-	if closeOnExit == nil then
-		closeOnExit = false
-	end
+    if closeOnExit == nil then
+        closeOnExit = false
+    end
 
-	local fileWithExtension = vim.fn.expand("%:p")
-	local fileWithoutExtension = vim.fn.fnamemodify(fileWithExtension, ":r")
+    local fileWithExtension = vim.fn.expand("%:p")
+    local fileWithoutExtension = vim.fn.fnamemodify(fileWithExtension, ":r")
 
-	local execute = {
-		python = function()
-			require("smart-floatterm").open('python "' .. fileWithExtension .. '"', closeOnExit)
-		end,
-		sh = function()
-			require("smart-floatterm").open('"' .. fileWithExtension .. '"', closeOnExit)
-		end,
-		c = function()
-			require("smart-floatterm").open(
-				'make "' .. fileWithoutExtension .. '" && ' .. '"' .. fileWithoutExtension .. '"',
-				closeOnExit
-			)
-		end,
-		lua = function()
-			require("smart-floatterm").open('luajit "' .. fileWithExtension .. '"', closeOnExit)
-		end,
-		dosbatch = function()
-			require("smart-floatterm").open('cmd /c "' .. fileWithExtension .. '" && exit', closeOnExit)
-		end,
-		ps1 = function()
-			require("smart-floatterm").open('powershell -File "' .. fileWithExtension .. '"', closeOnExit)
-		end,
-		html = function()
-			vim.system({ m.browser, fileWithExtension })
-		end,
-	}
-	execute[currentFiletype]()
+    local execute = {
+        python = function()
+            require("smart-floatterm").open('python "' .. fileWithExtension .. '"', closeOnExit)
+        end,
+        sh = function()
+            require("smart-floatterm").open('"' .. fileWithExtension .. '"', closeOnExit)
+        end,
+        c = function()
+            require("smart-floatterm").open(
+                'make "' .. fileWithoutExtension .. '" && ' .. '"' .. fileWithoutExtension .. '"',
+                closeOnExit
+            )
+        end,
+        lua = function()
+            require("smart-floatterm").open('luajit "' .. fileWithExtension .. '"', closeOnExit)
+        end,
+        dosbatch = function()
+            require("smart-floatterm").open('cmd /c "' .. fileWithExtension .. '" && exit', closeOnExit)
+        end,
+        ps1 = function()
+            require("smart-floatterm").open('powershell -File "' .. fileWithExtension .. '"', closeOnExit)
+        end,
+        html = function()
+            vim.system { m.browser, fileWithExtension }
+        end,
+    }
+    execute[currentFiletype]()
 end
 
 return m
