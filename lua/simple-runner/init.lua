82c732196bcfef5371cc53415f844712a5c82cea
ingenarel <ingenarelitems@gmail.com>
2025-05-02 10:24:10 +0600

    (feat): use opts

--- lua/simple-runner/init.lua
+++ lua/simple-runner/init.lua
@@ -6,13 +6,14 @@ function m.setup(opts)
     m.browser = opts.browser
 end
 
-function m.run(closeOnExit)
+function m.run(opts)
+    opts = opts or {}
     local currentFiletype = vim.bo.filetype
 
     vim.cmd("w")
 
-    if closeOnExit == nil then
-        closeOnExit = false
+    if opts.closeOnExit == nil then
+        opts.closeOnExit = false
     end
 
     local fileWithExtension = vim.fn.expand("%:p")
@@ -20,25 +21,31 @@ function m.run(closeOnExit)
 
     local execute = {
         python = function()
-            require("smart-floatterm").open { 'python "' .. fileWithExtension .. '"', closeOnExit = closeOnExit }
+            require("smart-floatterm").open { 'python "' .. fileWithExtension .. '"', closeOnExit = opts.closeOnExit }
         end,
         sh = function()
-            require("smart-floatterm").open { '"' .. fileWithExtension .. '"', closeOnExit = closeOnExit }
+            require("smart-floatterm").open { '"' .. fileWithExtension .. '"', closeOnExit = opts.closeOnExit }
         end,
         c = function()
             require("smart-floatterm").open {
                 'make "' .. fileWithoutExtension .. '" && ' .. '"' .. fileWithoutExtension .. '"',
-                closeOnExit = closeOnExit,
+                closeOnExit = opts.closeOnExit,
             }
         end,
         lua = function()
-            require("smart-floatterm").open { 'luajit "' .. fileWithExtension .. '"', closeOnExit = closeOnExit }
+            require("smart-floatterm").open { 'luajit "' .. fileWithExtension .. '"', closeOnExit = opts.closeOnExit }
         end,
         dosbatch = function()
-            require("smart-floatterm").open { 'cmd /c "' .. fileWithExtension .. '" && exit', closeOnExit = closeOnExit }
+            require("smart-floatterm").open {
+                'cmd /c "' .. fileWithExtension .. '" && exit',
+                closeOnExit = opts.closeOnExit,
+            }
         end,
         ps1 = function()
-            require("smart-floatterm").open { 'powershell -File "' .. fileWithExtension .. '"', closeOnExit = closeOnExit }
+            require("smart-floatterm").open {
+                'powershell -File "' .. fileWithExtension .. '"',
+                closeOnExit = opts.closeOnExit,
+            }
         end,
         html = function()
             vim.system { m.browser, fileWithExtension }
